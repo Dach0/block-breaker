cb7d4392f4071114102bc21fa92c90e1dc86c54f
Damjan Krivacevic <damjan.krivacevic@amplitudo.me>
2020-07-22 18:43:01 +0200

    first commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Security.Cryptography;
+
+public class GameSession : MonoBehaviour
+{
+
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length; //obrati pa\nju da je Objects...tra\imo koliko ih ukupno ima i yato je length
+      
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false); //ovo se stavlja zbog mogucih bagova koji se desava u milisekundama, treba da stoji kad god radimo singleton pattern
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+}
